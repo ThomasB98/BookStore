c67653b0e67d27a04d5b008cf22b8fa9ecb8393a
ThomasB98 <bansodthomas@gmail.com>
2024-12-09 16:45:15 +0530

    [Thomas] Created User Entity and Role ENUM

---
+++ ModelLayer/Model/Entity/User.cs
@@ -0,0 +1,43 @@
+﻿using ModelLayer.Model.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; } = "";
+
+        [Required]
+        [EmailAddress]
+        [StringLength(255)]
+        public string Email { get; set; } = "";
+
+        [Required]
+        public string Password { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+
+        [Required]
+        public bool IsActive { get; set; } = false;
+
+        [Required]
+        public Role role { get; set; } = Role.USER;
+
+        public string address { get; set; } = "";
+    }
+}
---
+++ ModelLayer/Model/Enums/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Role
+    {
+        USER,
+        ADMIN
+    }
+}
