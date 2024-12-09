19dc0f53abbc95603deacf099a94f43d805dbd39
ThomasB98 <bansodthomas@gmail.com>
2024-12-09 17:02:53 +0530

    [Thomas] Added WhishList and wishListItem Entity

---
+++ ModelLayer/Model/Entity/WishList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    [Table("wishlist")]
+    public class WishList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User? User { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+
+        public ICollection<wishListItem>? WishListItems { get; set; }
+    }
+}
---
+++ ModelLayer/Model/Entity/wishListItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Model.Entity
+{
+    [Table("wishlistitem")]
+    public class wishListItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public int WishListId {  get; set; }
+
+        [ForeignKey("WishListId")]
+        public WishList? WishList { get; set; }
+
+        [Required]
+        public int bookId { get; set; }
+    }
+}
