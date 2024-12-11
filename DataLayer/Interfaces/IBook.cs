2a745704da67cb662c0f0b881238b3ef99d6da8e
ThomasB98 <bansodthomas@gmail.com>
2024-12-11 15:47:08 +0530

    [Thomas] Created IBook interface and BookDL

---
+++ DataLayer/Interfaces/IBook.cs
@@ -0,0 +1,23 @@
+﻿using DataLayer.Utilities.ResponseBody;
+using ModelLayer.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Interfaces
+{
+    public interface IBook
+    {
+        Task<ResponseBody<IEnumerable<Book>>> GetAllBooksAsync();
+
+        Task<ResponseBody<Book?>> GetBookByIdAsync(int id);
+
+        Task<ResponseBody<Book>> AddBookAsync(Book book);
+
+        Task<ResponseBody<Book>> UpdateBookAsync(Book book);
+
+        Task<ResponseBody<bool>> DeleteBookAsync(int id);
+    }
+}
---
+++ DataLayer/Repository/BookDL.cs
@@ -0,0 +1,47 @@
+﻿using DataLayer.Interfaces;
+using DataLayer.Utilities.ResponseBody;
+using Microsoft.EntityFrameworkCore;
+using ModelLayer.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository
+{
+    public class BookDL : IBook
+    {
+        private readonly DbContext _context;
+
+        public BookDL(DbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<ResponseBody<IEnumerable<Book>>> GetAllBooksAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseBody<Book>> AddBookAsync(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseBody<bool>> DeleteBookAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseBody<Book?>> GetBookByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseBody<Book>> UpdateBookAsync(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
